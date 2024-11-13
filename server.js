a613b80a9b8acbcafab6d4e6aa4803a855eb610a
merine <merinemutitu@gmail.com>
2024-11-13 18:15:50 +0100

    added post, get, put, delete methods

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const Product = require ("./models/productModel")
 const app = express()
 const port = 3000
 
@@ -24,14 +25,118 @@ app.get('/', (req, res) => {
   })
 
 
+//fetch all products in the database
+  
+  app.get('/products',  async (req, res) => {
+    try {
+
+        const products = await Product.find({})
+        res.status(200).json(products);
+
+
+    }  catch (error) {
+
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+
+
+    }
+})
+       
+//fetch product by Id
+app.get('/products/:id',  async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id)
+        res.status(200).json(product);
+
+
+    }  catch (error) {
+
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+
+
+    }
+})
         
 
 
+//update a product
 
-  app.post('/product', (req, res) => {
-   console.log(req.body);
-   res.send(req.body);
+
+app.put('/products/:id',  async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id,  req.body)
+       
+
+        // we cannot find product to update
+     if (!product){
+        return res.status(404).json({message: `can't find the product with ${id}`});
+     }else {
+
+        const updatedProduct = await Product. findById(id)
+        res.status(200).json(updatedProduct);
+
+     }
+
+    }  catch (error) {
+
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+
+
+    }
+})
+        
+
+//add product to the database
+
+
+  app.post('/product',  async (req, res) => {
+    try {
+
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+
+
+    }  catch (error) {
+
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+
+
+    }
    
+    
+  }
+  
+)
+
+
+//delete product
+
+app.delete('/products/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id)
+        if (!product){
+            return res.status(404).json({message: `can't find the product with ${id}`});
+         }else {
+    
+            res.status(200).json(product);
+    
+         }
+    
+        }  catch (error) {
+    
+            console.log(error.message);
+            res.status(500).json({message: error.message})
+    
+    
+        }
+    
   }
   
 )
